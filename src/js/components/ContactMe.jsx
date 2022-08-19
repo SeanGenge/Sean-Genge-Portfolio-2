c467fd776cf9fd4cabd245d56d41a27a034d2de1
Sean Genge <seangenge@gmail.com>
2022-08-19 13:38:35 +1000

    Added contact me message

--- src/js/components/ContactMe.jsx
+++ src/js/components/ContactMe.jsx
@@ -12,6 +12,7 @@ class ContactMe extends React.Component {
 				<section id="contact-me">
 					<Container>
 						<div className="h1">Contact Me</div>
+						<p>You can contact me at seangenge@gmail.com, or you can use the links below</p>
 						<div className="socials">
 							<a href="mailto:seangenge@gmail.com" target="_blank"><i class="fa-solid fa-envelope"></i></a>
                             <a href="https://www.linkedin.com/in/seangenge/" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
