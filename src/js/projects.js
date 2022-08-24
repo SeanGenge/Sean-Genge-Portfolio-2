6627d68071d3e6d3371b7ae3ac126b433f9d242d
Sean Genge <seangenge@gmail.com>
2022-08-24 16:53:25 +1000

    Changed two projects to feature a deployed app

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,11 +1,19 @@
 const projectsList = [
+    // {
+    //     "name": "Wolfies",
+    //     "description": "View/search movie information",
+    //     "image": "Wolfies.png",
+    //     "github": "https://github.com/SeanGenge/Wolfies",
+    //     "deployed": null,
+    //     "languages": ["React", "JavaScript", "TheMovieDB API", "HTML", "CSS", "Bootstrap", "SCSS", "Node.js"]
+    // },
     {
-        "name": "Wolfies",
-        "description": "View/search movie information",
-        "image": "Wolfies.png",
-        "github": "https://github.com/SeanGenge/Wolfies",
-        "deployed": null,
-        "languages": ["React", "JavaScript", "TheMovieDB API", "HTML", "CSS", "Bootstrap", "SCSS", "Node.js"]
+        "name": "Github Readme Generator",
+        "description": "Simple UI for generating readme files",
+        "image": "readmeGenerator.png",
+        "github": "https://github.com/SeanGenge/Github-Readme-Generator",
+        "deployed": "https://generate-github-readme.herokuapp.com/",
+        "languages": ["JavaScript", "Heroku", "Handlebar.js", "HTML", "CSS", "express"]
     },
     {
         "name": "m&mDB (Movies and music db)",
@@ -16,6 +24,22 @@ const projectsList = [
         "languages": ["JavaScript", "TheMovieDB API", "TheAudioDB API", "HTML", "CSS", "MaterializeCSS"]
     },
     {
+        "name": "Weather App",
+        "description": "View weather information on any city",
+        "image": "Weather.png",
+        "github": "https://github.com/SeanGenge/Weather",
+        "deployed": "https://seangenge.github.io/Weather/",
+        "languages": ["HTML", "JavaScript", "Weather API", "CSS", "Bootstrap"]
+    },
+    {
+        "name": "Note taker",
+        "description": "Add/Save notes and revisit",
+        "image": "noteTaker.png",
+        "github": "https://github.com/SeanGenge/Note-taker",
+        "deployed": "https://sean-note-taker.herokuapp.com/notes",
+        "languages": ["JavaScript", "HTML", "CSS", "Heroku", "express"]
+    },
+    {
         "name": "Fill",
         "description": "Fill the board with one colour or make a pattern",
         "image": "Fill.png",
@@ -24,28 +48,12 @@ const projectsList = [
         "languages": ["React", "JavaScript", "HTML", "Babel", "Webpack", "CSS", "Node.js"]
     },
     {
-        "name": "Readme Generator using the CMD",
-        "description": "Generate github readme.md files",
-        "image": "readmeGenerator.png",
-        "github": "https://github.com/SeanGenge/readme_generator",
-        "deployed": null,
-        "languages": ["JavaScript", "CSS", "Node.js"]
-    },
-    {
         "name": "Work day scheduler",
         "description": "Add notes for today. Colour coded to see the current time",
         "image": "WorkDayScheduler.png",
         "github": "https://github.com/SeanGenge/Work-day-scheduler",
         "deployed": "https://seangenge.github.io/Work-day-scheduler/",
         "languages": ["HTML", "JavaScript", "CSS", "Moment.js", "Bootstrap"]
-    },
-    {
-        "name": "Weather App",
-        "description": "View weather information on any city",
-        "image": "Weather.png",
-        "github": "https://github.com/SeanGenge/Weather",
-        "deployed": "https://seangenge.github.io/Weather/",
-        "languages": ["HTML", "JavaScript", "Weather API", "CSS", "Bootstrap"]
     }
 ]
 
